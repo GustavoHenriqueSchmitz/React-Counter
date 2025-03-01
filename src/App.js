c8c19edcbf7f50fa2bb9392a8ce17378df10c8b0
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2025-03-01 12:03:14 -0300

    adding-vercel-analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css'
 import { Counter } from './components/Counter';
-import { Header } from './components/Header'
+import { Header } from './components/Header';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
-    <div id='counter-container'>
-      <Header imgDescription='Logo do react' title='React Counter'/>
-      <Counter/>
-    </div>
+    <>
+      <div id='counter-container'>
+        <Header imgDescription='Logo do react' title='React Counter'/>
+        <Counter/>
+      </div>
+      <Analytics/>
+    </>
   )
 }
 
